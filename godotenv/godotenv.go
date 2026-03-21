8954a22b3dd8c9abaa143090cf8c0ab43083abad
Elman Syah <elmansyah@192.168.1.7>
2026-03-21 22:37:12 +0700

    Golang Library

--- godotenv/godotenv.go
+++ godotenv/godotenv.go
@@ -10,21 +10,17 @@ func New() App {
 	return &Params{}
 }
 
-type Setup func() App
+type Init func() App
 
 func (*Params) Load() string {
 	if err := godotenv.Load(".env"); err != nil {
 		panic("failed to load .env: " + err.Error())
-		
-		return ""
 	}
 	
 	appMode := os.Getenv("APP_MODE")
 	
 	if appMode == "" {
 		panic("APP MODE environment variable is not set")
-		
-		return ""
 	}
 	
 	return appMode
